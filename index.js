7749d1d450750f8c3423b7e2b8498ef3cf36189a
Leonardo Reyes <reyesleonardo44@yahoo.com>
2023-03-22 19:20:07 -0700

    Loops lab is complete

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+/* for (let age = 30; age < 40; age ++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+const getMessage = [];
+
+function writeCards(names, eventNames) {
+    for (let i = 0; i < names.length; i++) {
+        let message = `Thank you, ${names[i]}, for the wonderful ${eventNames} gift!`;
+        getMessage.push(message);
+        //debugger;
+    }
+    return getMessage;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
